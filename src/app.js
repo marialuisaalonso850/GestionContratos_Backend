008f268a26ceb0e2ed2c06ad959cdd89e0073e05
Stevenz02 <jssmorales51@gmail.com>
2025-06-27 16:09:59 -0500

    Implementación CORS
    Se realiza implementación de CORS

--- src/app.js
+++ src/app.js
@@ -1,37 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const swaggerUi = require('swagger-ui-express')
-const swaggerSpec = require('../swagger')
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../swagger');
 const config = require('./modules/Contratos/config/index');
 const abogadoRoutes = require('./modules/Contratos/routes/routes.crearAbogado');
-const procesosRoutes = require('./modules/Contratos/routes/routes.procesos')
+const procesosRoutes = require('./modules/Contratos/routes/routes.procesos');
 const tipoRoutes = require('./modules/Contratos/routes/routes.TipoContrato');
 const LoginRoutes = require('./modules/Contratos/routes/routes.login');
-const ContratosRoutes = require('./modules/Contratos/routes/routes.contratos')
-const lectura = require('./modules/Contratos/routes/routes.leerArchivo')
+const ContratosRoutes = require('./modules/Contratos/routes/routes.contratos');
+const lectura = require('./modules/Contratos/routes/routes.leerArchivo');
+const cors = require('cors'); // ✅ IMPORTAR
 const router = express.Router();
 const os = require('os');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json()); 
+app.use(cors()); // ✅ HABILITAR CORS para todas las rutas
 
+app.use(express.json());
 
 mongoose.connect(config.mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('Conectado a MongoDB :)'))
-.catch((err) => console.error(' Error de conexión:', err));
+.catch((err) => console.error('Error de conexión:', err));
 
-app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/abogados', abogadoRoutes);
 app.use('/api/procesos', procesosRoutes);
 app.use('/api/tipoContrato', tipoRoutes);
-app.use('/api/auth', LoginRoutes); 
+app.use('/api/auth', LoginRoutes);
 app.use('/api/contrato', ContratosRoutes);
-app.use('/api/datos', lectura)
+app.use('/api/datos', lectura);
 
 module.exports = app;
